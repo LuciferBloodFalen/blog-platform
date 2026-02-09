5d278e84569532afea388fe83b16a79d762ed6c3
LuciferBloodFalen <karunakarraunak@gmail.com>
2026-02-09 22:05:13 +0530

    Created project structure for backend

---
+++ backend/config/settings/dev.py
@@ -0,0 +1,7 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-$@k4@+n4awwz=fq3t@(y%z*xtxv*@=sx9c#_-4)ev%ltoy^asd'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
